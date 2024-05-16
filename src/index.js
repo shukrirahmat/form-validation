09a4d6762171fcd7dc502a9895d81dafa27f1d18
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-05-17 04:54:34 +0800

    done with email

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const form = document.querySelector("form");
+const mail = document.querySelector("#mail");
+const mailerror = document.querySelector("#mail + p.error");
+
+const showMailError = function showMailError() {
+  if (mail.validity.valueMissing) {
+    mailerror.textContent = "Email address is required";
+  } else if (mail.validity.typeMismatch) {
+    mailerror.textContent = "Email adress must be valid";
+  }
+};
+
+mail.addEventListener("input", (event) => {
+  if (mail.validity.valid) {
+    mailerror.textContent = "";
+  } else {
+    showMailError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!mail.validity.valid) {
+    showMailError();
+    event.preventDefault();
+  }
+});
+
+
