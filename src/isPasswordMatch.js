daf3e595cb5c9121c66ff926a0bcd557ce59d826
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-05-17 14:05:01 +0800

    Initial dist subtree commit

--- src/isPasswordMatch.js
+++
@@ -1,8 +0,0 @@
-const pwinput = document.querySelector("#pw");
-const pwconfirminput = document.querySelector("#pwconfirm");
-
-const isPasswordMatch = function isPasswordMatch() {
-  return (pwinput.value === pwconfirminput.value);
-};
-
-export default isPasswordMatch;
